87174606ac84cab45624f7eeaadd4f55cfec4b81
renan.maciel <renan.maciel@oncoclinicas.com>
2025-01-15 15:57:15 -0300

    Fix: corrigindo erros de pontuação no user name e melhorando resiliencia

--- gmail.py
+++ gmail.py
@@ -44,11 +44,11 @@ def account_input_name(bot: WebBot, full_name: str):
         name = full_name.split(' ')
 
         # Preencher nome
-        bot.find_element("//input[@id='firstName']", By.XPATH).send_keys(name[0])
+        bot.find_element("//input[@id='firstName']", By.XPATH, ensure_visible=True, ensure_clickable=True).send_keys(name[0])
         # sobrenome
-        bot.find_element("//input[@id='lastName']", By.XPATH).send_keys(name[2])
+        bot.find_element("//input[@id='lastName']", By.XPATH, ensure_visible=True, ensure_clickable=True).send_keys(name[2])
         # clica em próximo
-        bot.find_element("//span[text()='Próxima']", By.XPATH).click()
+        bot.find_element("//span[text()='Próxima']", By.XPATH, ensure_visible=True, ensure_clickable=True).click()
 
         # validar carregamento da próxima página:
         if not bot.find_element("//select[@id='gender']", By.XPATH, waiting_time=30000):  # espera 30 segundos
@@ -63,15 +63,15 @@ def account_input_age_gender(bot: WebBot, genero: str):
 
         try:
             # Preencher idade
-            bot.find_element("//input[@id='day']", By.XPATH).send_keys('26')  # Dia
-            element_as_select(bot.find_element("//select[@id='month']", By.XPATH)).select_by_visible_text('Janeiro')  # Mês
-            bot.find_element("//input[@id='year']", By.XPATH).send_keys('1995')  # Ano
+            bot.find_element("//input[@id='day']", By.XPATH, ensure_visible=True, ensure_clickable=True).send_keys('26')  # Dia
+            element_as_select(bot.find_element("//select[@id='month']", By.XPATH, ensure_visible=True, ensure_clickable=True)).select_by_visible_text('Janeiro')  # Mês
+            bot.find_element("//input[@id='year']", By.XPATH, ensure_visible=True, ensure_clickable=True).send_keys('1995')  # Ano
 
             # Selecionar gênero
-            element_as_select(bot.find_element("//select[@id='gender']", By.XPATH)).select_by_visible_text(genero)
+            element_as_select(bot.find_element("//select[@id='gender']", By.XPATH, ensure_visible=True, ensure_clickable=True)).select_by_visible_text(genero)
 
             # clica em próximo
-            bot.find_element("//span[text()='Próxima']", By.XPATH).click()
+            bot.find_element("//span[text()='Próxima']", By.XPATH, ensure_visible=True, ensure_clickable=True).click()
 
             # validar carregamento da próxima página:
             if not bot.find_element("//input[@name='Username']", By.XPATH, waiting_time=30000):  # espera até 30 segundos
@@ -85,13 +85,18 @@ def account_input_age_gender(bot: WebBot, genero: str):
 def account_username(bot: WebBot, full_name):
 
     try:
+        # Settings das variaveis
         number_aleatory = random.randint(1000, 100000)
+        full_name = full_name.replace(' ', '.')
 
         # Preencher nome de usuário
-        bot.find_element("//input[@name='Username']", By.XPATH).send_keys(f'{full_name}{number_aleatory}')
+        bot.find_element("//input[@name='Username']", By.XPATH, ensure_visible=True, ensure_clickable=True).send_keys(f'{full_name}{number_aleatory}')
+
+        # clica em próximo
+        bot.find_element("//span[text()='Próxima']", By.XPATH, ensure_visible=True, ensure_clickable=True).click()
 
         # validar carregamento da próxima página:
-        if not bot.find_element("//input[@name='Passwd']", By.XPATH, waiting_time=30000):  # espera até 30 segundos
+        if not bot.find_element("//input[@name='Passwd']", By.XPATH, ensure_visible=True, ensure_clickable=True, waiting_time=30000):  # espera até 30 segundos
             raise Exception("Falha ao carregar a proxima pagina")
 
     except Exception as error:
@@ -103,14 +108,17 @@ def account_password(bot: WebBot, password: str):
 
     try:
         # Digita a senha
-        bot.find_element("//input[@name='Passwd']", By.XPATH).send_keys(password)
+        bot.find_element("//input[@name='Passwd']", By.XPATH, ensure_visible=True, ensure_clickable=True).send_keys(password)
         bot.wait(10000)
 
         # Confirma a senha
-        bot.find_element("//input[@name='PasswdAgain']", By.XPATH).send_keys(password)
+        bot.find_element("//input[@name='PasswdAgain']", By.XPATH, ensure_visible=True, ensure_clickable=True).send_keys(password)
+
+        # clica em próximo
+        bot.find_element("//span[text()='Próxima']", By.XPATH, ensure_visible=True, ensure_clickable=True).click()
 
         # validar carregamento da próxima página:
-        if not bot.find_element("//input[@name='Passwd']", By.XPATH, waiting_time=30000):  # espera até 30 segundos
+        if not bot.find_element("//input[@name='Passwd']", By.XPATH, ensure_visible=True, ensure_clickable=True, waiting_time=30000):  # espera até 30 segundos
             raise Exception("Falha ao carregar a proxima pagina")
 
     except Exception as error:
